0a690b798b0c4d74e47b584991c6736747c3d582
markramirez2613 <markramirez2613@gmail.com>
2024-10-20 17:40:18 +0800

    Testing Loan Calculator

--- resources/views/livewire/admin/loan-calculator.blade.php
+++ resources/views/livewire/admin/loan-calculator.blade.php
@@ -3,55 +3,80 @@
 use Livewire\Volt\Component;
 
 new class extends Component {
-    public $result;
-    public $a;
-    public $b = 2;
+    public $totalprincipalAmount;
+    public $principalAmount;
+    public $termsYears;
     public $terms;
+    public $monthlyInstallment;
+    public $annualInterestRate;
+    public $monthlyInterestRate;
+    
 
+    public $otherChargesInterest = 0.06;
  
-    public function save()
+    public function mount()
     {
-        $this->result = ($this->a ?? 0) * ($this->terms[['year']] ?? 0);
+        $this->terms = [
+            [
+                'id' => 1,
+                'name' => '1 Year'
+            ],
+            [
+                'id' => 2,
+                'name' => '2 Years',
+            ],
+            [
+                'id' => 3,
+                'name' => '3 Years',
+            ],
+            [
+                'id' => 4,
+                'name' => '4 Years',
+            ],
+            [
+                'id' => 5,
+                'name' => '5 Years',
+            ]
+        ];
+    }
+
+    public function compute()
+    {
+
+        $this->monthlyInstallment = $this->termsYears * 12;
+        $this->totalprincipalAmount = number_format($this->principalAmount, 2, '.', ',');
+        dd($this->totalprincipalAmount);
+
+        if($this->termsYears == 5 || $this->termsYears == 4 || $this->termsYears == 3) {
+            $this->annualInterestRate = 0.0966;
+            $this->monthlyInterestRate = $this->annualInterestRate/12;
+        } else if($this->termsYears == 2) {
+            $this->annualInterestRate = 0.0900;
+            $this->monthlyInterestRate = $this->annualInterestRate/12;
+        } else {
+            $this->annualInterestRate = 0.0750;
+            $this->monthlyInterestRate = $this->annualInterestRate/12;
+        }
+
     }
     
 }; ?>
 
 <div>
 
-        <x-form wire:submit.prevent="save">
-            <x-input label="Amount" wire:model.live="a" prefix="PHP" money hint="It submits an unmasked value" />
+    <x-form wire:submit.prevent="compute">
+        <x-input label="Principal Amount" wire:model="principalAmount" prefix="PHP" money hint="It submits an unmasked value" />
+        <x-select label="Terms" :options="$terms" wire:model="termsYears" />
+        <x-slot:actions>
+            <x-button label="Cancel" />
+            <x-button label="Click me!" class="btn-primary" type="submit" spinner="save" />
+        </x-slot:actions>
+    </x-form>
 
-            @php
-                $terms = [
-                    [
-                        'year' => 1,
-                        'name' => '1 Year'
-                    ],
-                    [
-                        'year' => 2,
-                        'name' => '2 Years',
-                    ],
-                    [
-                        'year' => 3,
-                        'name' => '3 Years',
-                    ],
-                    [
-                        'year' => 4,
-                        'name' => '4 Years',
-                    ],
-                    [
-                        'year' => 5,
-                        'name' => '5 Years'
-                    ]
-                ];
-            @endphp
- 
-            <x-select label="Terms " :options="$terms" wire:model="selectedTerms" />
-        
-            <x-slot:actions>
-                <x-button label="Cancel" />
-                <x-button label="Click me!" class="btn-primary" type="submit" spinner="save" />
-            </x-slot:actions>
-        </x-form>
-        <!-- Sonuç : {{ $result }} -->
+
+    {{ $totalprincipalAmount }} <br>
+    {{ $monthlyInstallment }} <br><br>
+
+    {{$annualInterestRate}}
+    {{$monthlyInterestRate}}
 </div>
