10351218847f76a6cf7855d10dab55182e20a191
markramirez2613 <markramirez2613@gmail.com>
2024-10-20 19:34:45 +0800

    updateloancalculator

--- resources/views/livewire/admin/loan-calculator.blade.php
+++ resources/views/livewire/admin/loan-calculator.blade.php
@@ -10,9 +10,28 @@ new class extends Component {
     public $monthlyInstallment;
     public $annualInterestRate;
     public $monthlyInterestRate;
+
+    public $otherChargesInterest = 0.06; 
+    public $otherCharges;
+    public $netProceeds;
+    public $totalNetProceeds;
+    public $monthlyPayment;
+    public $totalMonthlyPaymentRound;
+    public $totalMonthlyPayment;
+
+    public $outstandingBalance;
+    public $installmentDetails = array();
+
+    public $month;
+    public $interest;
+    public $principalPaid;
+    public $netPay;
+    public $Charges;
+    public $totalOtherCharges;
+
+
     
 
-    public $otherChargesInterest = 0.06;
  
     public function mount()
     {
@@ -38,6 +57,9 @@ new class extends Component {
                 'name' => '5 Years',
             ]
         ];
+
+
+        
     }
 
     public function compute()
@@ -45,7 +67,7 @@ new class extends Component {
 
         $this->monthlyInstallment = $this->termsYears * 12;
         $this->totalprincipalAmount = number_format($this->principalAmount, 2, '.', ',');
-        dd($this->principalAmount);
+
 
         if($this->termsYears == 5 || $this->termsYears == 4 || $this->termsYears == 3) {
             $this->annualInterestRate = 0.0966;
@@ -58,6 +80,37 @@ new class extends Component {
             $this->monthlyInterestRate = $this->annualInterestRate/12;
         }
 
+        
+        $this->otherChargesInterest = 0.06; //6%
+        $this->otherCharges = $this->principalAmount * $this->otherChargesInterest;
+        $this->totalOtherCharges = number_format($this->otherCharges, 2, '.', ',');
+        $this->netProceeds = $this->principalAmount - $this->otherCharges;
+        $this->totalNetProceeds = number_format($this->netProceeds, 2, '.', ',');
+        $this->monthlyPayment = $this->principalAmount * ($this->monthlyInterestRate * ((1 + $this->monthlyInterestRate) ** $this->monthlyInstallment)) / ((1 + $this->monthlyInterestRate) ** $this->monthlyInstallment - 1);
+        $this->totalMonthlyPaymentRound = round($this->monthlyPayment,2);
+        $this->totalMonthlyPayment = number_format($this->totalMonthlyPaymentRound, 2, '.', ',');
+
+        for ($this->month = 1; $this->month <= $this->monthlyInstallment; $this->month++) {
+            $this->interest = $this->outstandingBalance * $this->monthlyInterestRate;
+            $this->principalPaid = $this->monthlyPayment - $this->interest;
+            $this->Charges = $this->principalAmount * $this->otherChargesInterest;
+            $this->netProceeds = -($this->principalPaid + $this->interest);
+            $this->netPay = $this->principalAmount - $this->Charges;
+            // Update outstanding balance for the next iteration
+            $this->outstandingBalance = $this->outstandingBalance - $this->principalPaid;
+            // Store installment details in the array
+            $this->installmentDetails[] = array(
+                'month' => $this->month,
+                'grossLoan' => number_format($this->principalAmount, 2, '.', ','),
+                'principal' => number_format($this->principalPaid, 2, '.', ','),
+                'interest' => number_format($this->interest, 2, '.', ','),
+                'netProceeds' => number_format($this->netProceeds, 2, '.', ','),
+                'outstandingBalance' => number_format($this->outstandingBalance, 2, '.', ','),
+            );
+        }
+
+        dd($this->installmentDetails);
+
     }
     
 }; ?>
@@ -66,7 +119,7 @@ new class extends Component {
 
     <x-form wire:submit.prevent="compute">
         <x-input label="Principal Amount" wire:model.defer="principalAmount" prefix="PHP" money hint="It submits an unmasked value" />
-        <x-select label="Terms" :options="$terms" wire:model="termsYears" />
+        <x-select label="Terms" :options="$terms" wire:model="termsYears" placeholder="Select Terms"/>
         <x-slot:actions>
             <x-button label="Cancel" />
             <x-button label="Click me!" class="btn-primary" type="submit" spinner="save" />
@@ -74,9 +127,17 @@ new class extends Component {
     </x-form>
 
 
-    {{ $totalprincipalAmount }} <br>
-    {{ $monthlyInstallment }} <br><br>
+    Principal Amount {{ $totalprincipalAmount }} <br>
+    No of Installments: {{ $monthlyInstallment }} <br><br>
+    
+     Other Charges:  {{$totalOtherCharges}} <br>
 
-    {{$annualInterestRate}}
-    {{$monthlyInterestRate}}
+
+     Annual Interest Rate: {{$annualInterestRate}} <br>
+     Monthly Interest Rate: {{$monthlyInterestRate}}
+
+
+ 
+ 
+
 </div>
