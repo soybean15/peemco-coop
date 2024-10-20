99421e6fdd30e9334cc7b17b42382c2e31524aef
soybean15 <marlonpadilla1593@gmail.com>
2024-10-20 15:44:24 +0800

    Update- updated routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,7 +24,13 @@ return Application::configure(basePath: dirname(__DIR__))
         }
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            // 'role' => \App\Http\Middleware\RoleMiddleware::class,
+        
+
+
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
