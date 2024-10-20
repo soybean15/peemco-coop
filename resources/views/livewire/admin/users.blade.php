b12fc9dfd22f738832e8c6326b74a3aafef9e719
soybean15 <marlonpadilla1593@gmail.com>
2024-10-20 17:22:54 +0800

    Update -added user component

--- resources/views/livewire/admin/users.blade.php
+++ resources/views/livewire/admin/users.blade.php
@@ -1,11 +1,41 @@
 <?php
 
 use Livewire\Volt\Component;
-
+use App\Models\User;
+use Livewire\WithPagination;
 new class extends Component {
-    //
+    use WithPagination;
+    public $search;
+    public function with(){
+        return [
+            
+            'userHeaders'=>[
+        ['key' => 'id', 'label' => '#', 'class' => 'bg-red-500/20 w-1'],
+        ['key' => 'name', 'label' => 'Username'],
+        ['key' => 'email', 'label' => 'E-mail', 'class' => 'hidden lg:table-cell'], 
+        ['key' => 'action', 'label' => 'Action', 'class' => 'hidden lg:table-cell'], 
+
+         // Alternative approach
+    ],
+            'users'=>User::search($this->search)->paginate(5)
+    ];
+    }
+
+    
 }; ?>
 
 <div>
-    //
+    <x-header title="Users" subtitle="Your home address" separator>
+
+        <x-slot:actions>
+            <x-input icon="o-magnifying-glass" wire:model.live='search' placeholder="Search..." />
+        </x-slot:actions>
+    </x-header>
+    <x-table :headers="$userHeaders" :rows="$users" with-pagination >
+
+        @scope('cell_action', $user)
+        <x-button icon='o-eye' class="btn-error btn-sm"/>
+        <x-button icon='o-archive-box' class="btn-error btn-sm"/>
+        @endscope
+    </x-table>
 </div>
