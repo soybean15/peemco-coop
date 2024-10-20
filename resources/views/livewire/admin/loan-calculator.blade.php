9622fd5807a6dfa8cd38f2e2fc193abeb55d09aa
markramirez2613 <markramirez2613@gmail.com>
2024-10-20 15:50:39 +0800

    pull

--- resources/views/livewire/admin/loan-calculator.blade.php
+++ resources/views/livewire/admin/loan-calculator.blade.php
@@ -3,9 +3,25 @@
 use Livewire\Volt\Component;
 
 new class extends Component {
-    //
+    public $result;
+    public $a;
+    public $b;
+    
+    public function updated()
+    {
+        $this->result = ($this->a ?? 0) + ($this->b ?? 0);
+    }
+    
 }; ?>
 
 <div>
-    //
+    <label>Sayı 1:</label>
+        <input type="text" id="a" name="a" wire:model.lazy="a">
+
+        <br>
+        <label>Sayı 2:</label>
+        <input type="text" id="b" name="b" wire:model.lazy="b">
+        
+        <br>
+        Sonuç : {{ $result }}
 </div>
--- resources/views/livewire/user/dashboard.blade.php
+++ resources/views/livewire/user/dashboard.blade.php
@@ -3,9 +3,10 @@
 use Livewire\Volt\Component;
 
 new class extends Component {
-    //
+   
 }; ?>
 
 <div>
-    DashBoard
+   
+
 </div>
