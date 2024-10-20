4eaf18178bd0e16224759ddcd06313b63fd6737d
markramirez2613 <markramirez2613@gmail.com>
2024-10-20 16:14:41 +0800

    4:14-10-20-2024

--- resources/views/livewire/admin/loan-calculator.blade.php
+++ resources/views/livewire/admin/loan-calculator.blade.php
@@ -5,23 +5,53 @@ use Livewire\Volt\Component;
 new class extends Component {
     public $result;
     public $a;
-    public $b;
-    
-    public function updated()
+    public $b = 2;
+    public $terms;
+
+ 
+    public function save()
     {
-        $this->result = ($this->a ?? 0) + ($this->b ?? 0);
+        $this->result = ($this->a ?? 0) * ($this->terms[['year']] ?? 0);
     }
     
 }; ?>
 
 <div>
-    <label>Sayı 1:</label>
-        <input type="text" id="a" name="a" wire:model.lazy="a">
 
-        <br>
-        <label>Sayı 2:</label>
-        <input type="text" id="b" name="b" wire:model.lazy="b">
+        <x-form wire:submit.prevent="save">
+            <x-input label="Amount" wire:model.live="a" prefix="PHP" money hint="It submits an unmasked value" />
+
+            @php
+                $terms = [
+                    [
+                        'year' => 1,
+                        'name' => '1 Year'
+                    ],
+                    [
+                        'year' => 2,
+                        'name' => '2 Years',
+                    ],
+                    [
+                        'year' => 3,
+                        'name' => '3 Years',
+                    ],
+                    [
+                        'year' => 4,
+                        'name' => '4 Years',
+                    ],
+                    [
+                        'year' => 5,
+                        'name' => '5 Years'
+                    ]
+                ];
+            @endphp
+ 
+            <x-select label="Terms " :options="$terms" wire:model="selectedTerms" />
         
-        <br>
+            <x-slot:actions>
+                <x-button label="Cancel" />
+                <x-button label="Click me!" class="btn-primary" type="submit" spinner="save" />
+            </x-slot:actions>
+        </x-form>
         Sonuç : {{ $result }}
 </div>
