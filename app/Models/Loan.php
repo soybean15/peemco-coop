527d74eaceee27abcb689508aed5ab8b0837ed31
soybean15 <marlonpadilla1593@gmail.com>
2024-10-24 19:03:09 +0800

    Update -added loan list

--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Livewire\WithPagination;
 
 class Loan extends Model
 {
     //
+
     protected $guarded=[];
+
+    public function scopeSearch(Builder $builder,$search){
+        if(empty($search))
+        return $builder;
+
+        $builder->where('loan_application_no',$search);
+
+
+    }
 }
