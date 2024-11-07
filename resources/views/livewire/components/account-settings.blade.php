fc95b4f7601d0f47df86768698e7f39b29c1d757
markramirez2613 <markramirez2613@gmail.com>
2024-11-07 20:47:48 +0800

    update

--- resources/views/livewire/components/account-settings.blade.php
+++ resources/views/livewire/components/account-settings.blade.php
@@ -3,17 +3,98 @@
 use Livewire\Volt\Component;
 use App\Models\User;
 
+use Illuminate\Validation\Rules;
+
 new class extends Component {
     public $user;
+    public string $name = '';
+    public string $middlename = '';
+    public string $lastname = '';
+    public string $username = '';
+    public string $email = '';
+    public string $password = '';
+    public string $password_confirmation = '';
 
-    public function mount($user_id){
-
+    public function mount($user_id)
+    {
         $this->user = User::find($user_id);
+        $this->name = $this->user->name;
+        $this->middlename = $this->user->middlename;
+        $this->lastname = $this->user->lastaname;
+        $this->username = $this->user->username;
+        $this->email = $this->user->email;
+        $this->password = $this->user->password;
+   
+    }
 
+    public function updateAccount()
+    {
+      
     }
 }; ?>
 
 <div>
     {{$user->id}}
-    Test
+    <form wire:submit="updateAccount">
+        <!-- Name -->
+        <div>
+            <x-input-label for="name" :value="__('Name')" />
+            <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+
+        <!-- Lastname -->
+        <div>
+            <x-input-label for="lastname" :value="__('Lastname')" />
+            <x-text-input wire:model="lastname" id="lastname" class="block mt-1 w-full" type="text" name="lastname" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('lastname')" class="mt-2" />
+        </div>
+
+        <!-- Username -->
+        <div>
+            <x-input-label for="username" :value="__('Username')" />
+            <x-text-input wire:model="username" id="username" class="block mt-1 w-full" type="text" name="username" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('username')" class="mt-2" />
+        </div>
+
+
+        <!-- Email Address -->
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input wire:model="password" id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="mt-4">
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+            <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
+                            type="password"
+                            name="password_confirmation" required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <x-primary-button class="ms-4">
+                    {{ __('Update') }}
+            </x-primary-button>
+        </div>
+        
+    </form>
+
 </div>
+
