56cafa3423b3986842bdb9a5276ea9d0b20a6956
markramirez2613 <markramirez2613@gmail.com>
2024-11-09 21:57:14 +0800

    updateprofile

--- resources/views/livewire/user/profile.blade.php
+++ resources/views/livewire/user/profile.blade.php
@@ -1,29 +1,17 @@
 <?php
 
 use Livewire\Volt\Component;
-use App\Models\User;
 
 
 
 new class extends Component {
-    public $user;
-
-    public function mount()
-    {
-        $this->user = Auth()->id();
-    }
-
+ 
 
 
 }; ?>
 
 <div>
-<div class="p-3 m-1 border-2 rounded shadow-md ">
-        <div class="flex flex-col justify-start md:flex-row">
+    <livewire:components.user-profile :user="auth()->user()"/>
 
-            {{$user}}
-           
-        </div>
-    </div>
     
 </div>
