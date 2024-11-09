27ff387ad5d8455342e6d0782654a670ad603706
soybean15 <marlonpadilla1593@gmail.com>
2024-11-09 21:42:33 +0800

    update - created reusable userprofile

--- resources/views/livewire/admin/user/profile.blade.php
+++ resources/views/livewire/admin/user/profile.blade.php
@@ -22,22 +22,13 @@ new class extends Component {
 
     public function mount()
     {
+
+ 
         $this->form = $this->user->profile->toArray();
     }
 
 
-    public function save(){
 
-        try{
-            $this->user->profile->update($this->form);
-            session()->flash('success','Profile Updated Successfully');
-            $this->redirect(route('admin.users', absolute: false), navigate: true);
-
-        }catch(\Exception $e){
-            dd($e);
-
-        }
-    }
 
 }; ?>
 <div>
@@ -62,78 +53,10 @@ new class extends Component {
             <div class="flex flex-col w-full px-5 md:p-5">
                 <x-tabs wire:model="selectedTab">
                     <x-tab name="profile" label="Profile" icon="o-users">
-                    <x-header title="{{html_entity_decode($user->name)}} {{html_entity_decode($user->middlename)}} {{html_entity_decode($user->lastname)}}" subtitle="{{$user->email}}" separator />
 
-                    <div>
-                        <label class="label">
-                            <span class="text-base label-text">Member ID No:   <b>{{html_entity_decode($user->mid)}}</b></span>
-                        </label>
+                        <livewire:components.user-profile :user="$user"/>
 
-                    </div>
-                    <div>
-                        <x-input label="TIN (Tax Identification Number): " wire:model="form.tin_no" />
-                    </div>
-                    <br>
-                    <hr>
-                    <br>
-                    <h6><b>I. Information on the Membership upon acceptance:</b></h6>
-                    <br>
-                    <div>
-                        <x-datetime label="Date Accepted:" wire:model="form.date_accepted" icon="o-calendar"/>
-
-                        <x-input label="Board of Directors (BOD) Resolution Number: " wire:model="form.acceptance_membership_bod_resolution_no" />
-                
-                        <x-input label="Type/Kind of Membership: " wire:model="form.type_of_membership" />
-                  
-                        <br>
-
-                        <b><span class="text-base label-text">Initial Capital Subscription</span></b>
-
-                        <x-input label="Number of Share: " wire:model="form.number_of_share" />
-               
-                        <x-input label="Amount: " wire:model="form.amount" />
-                     
-                        <x-input label="Initial Paid Up: " wire:model="form.initial_paid_up" />
-                   
-
-                    </div>
-                    <br>
-                    <h6><b>II. Membership Profile:</b></h6>
-                    <br>
-                    <x-input label="Address: " wire:model="form.address" />
-            
-                    <x-datetime label="Date of Birth:" wire:model="form.date_of_birth" icon="o-calendar"/>
-              
-                    <x-input label="Gender: " wire:model="form.gender" />
-                 
-                    <x-input label="Civil Status: " wire:model="form.civil_status" />
-        
-                    <x-input label="Highest Educational Attainment: " wire:model="form.highest_educational_attainment" />
-               
-                    <x-input label="Occupation/Income Source: " wire:model="form.occupation_income_source" />
-                   
-                    <x-input label="Number of Dependents:" wire:model="form.number_of_dependents" />
-                   
-                    <x-input label="Religion/Social Affiliation:" wire:model="form.religion_social_affiliation" />
-                  
-
-                    <x-input label="Annual Income:" wire:model="form.annual_income" />
-                    
-
-
-                    <hr>
-                    <br>
-                    <h6><b>III. Termination of Membership:</b></h6>
-                    <br>
-                    <x-input label="BOD Resolution Number:" wire:model="form.termination_membership_bod_resolution_no" />
-                
-                    <x-datetime label="Date:" wire:model="form.termination_membership_date" icon="o-calendar"/>
-                    <br>
-                    <div class="flex justify-end px-5">
-                        <x-button label='Save' class="btn-primary btn-sm" wire:click='save'/>
-                    </div>
-
-                </x-tab>
+                    </x-tab>
 
                 <x-tab name="capital_build_up" label="Capital Build up" icon="o-arrow-trending-up">
                    <livewire:components.capital-build-up-list :user_id='$user->id'/>
