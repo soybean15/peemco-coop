bb587f78fd8f1ef849536df5aa8b3863c1a40ac9
soybean15 <marlonpadilla1593@gmail.com>
2024-11-11 09:25:45 +0800

    Update added search user

--- app/Support/Spotlight.php
+++ app/Support/Spotlight.php
@@ -9,9 +9,30 @@ class Spotlight
 {
     public function search(Request $request)
     {
+        if(! auth()->user()) {
+            return [];
+        }
 
-        return User::search($request->search)
-        ->take(5)->get();
+
+        return collect()
+        // ->merge($this->actions($request->search))
+        ->merge($this->users($request->search));
 
     }
+    public function users(string $search = '')
+    {
+        return User::query()
+                ->where('name', 'like', "%$search%")
+                ->take(5)
+                ->get()
+                ->map(function (User $user) {
+                    return [
+                        // 'avatar' => $user->profile_picture,
+                        'name' => $user->name,
+                        'description' => $user->email,
+                        'link' => "/admin/users/{$user->id}"
+                    ];
+                });
+    }
+
 }
