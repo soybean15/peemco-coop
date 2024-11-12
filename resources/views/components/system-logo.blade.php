ea216efea78615b2817de871928f2c36273941c9
markramirez2613 <markramirez2613@gmail.com>
2024-11-12 21:06:17 +0800

    profilepic

--- resources/views/components/system-logo.blade.php
+++ resources/views/components/system-logo.blade.php
@@ -5,6 +5,7 @@
     <div class="{{ $class }}">
         <img src="{{ $settings->logo ?? '/default/default-logo.png' }}" class="rounded-lg w-{{ $size }}" />
 
+        {{$settings->logo }}
         @if($hasName)
             <div>
                 {{ $settings->company_name }}
