2d5f5d64e500ea1f11b84bc563598fd820dc7dff
soybean15 <marlonpadilla1593@gmail.com>
2024-11-18 21:28:51 +0800

    Update - modified loan profile

--- resources/views/livewire/components/loan-profile.blade.php
+++ resources/views/livewire/components/loan-profile.blade.php
@@ -83,67 +83,58 @@ new class extends Component {
 
 <div>
 
-    <x-header title="Loan Details"  separator >
+    <div class="p-6 border rounded">
+        <x-header title="Loan Details"  separator>
 
-        <x-slot:actions>
+            <x-slot:actions>
 
-        @if($loan->status=='pending')
-        @can('approve loan')
-        <x-button class="btn-success" label='Approve' wire:confirm='Are you sure you want to approve this loan?'
-        wire:click='approveLoan' />
-    <x-button class="btn-error" label='Reject' wire:confirm='Are you sure you want to reject this loan?'
-        wire:click='rejectLoan' />
-        @endcan
-
-
-        @else
-
-            @if($this->loan->status =='rejected')
-            <x-icon name="o-x-mark" class="text-2xl text-red-500 w-9 h-9" label="{{  $this->loan->status }}" />
-
-            @else
-
-            <x-icon name="o-check" class="text-2xl text-green-500 w-9 h-9" label="{{  $this->loan->status }}" />
-
-            @endif
+                @if($loan->status=='pending')
+                <x-icon name="o-clock" class="text-2xl text-warning w-9 h-9" label="{{  $this->loan->status }}" />
+                @can('approve loan')
+                <x-button class="btn-success" label='Approve' wire:confirm='Are you sure you want to approve this loan?'
+                    wire:click='approveLoan' />
+                <x-button class="btn-error" label='Reject' wire:confirm='Are you sure you want to reject this loan?'
+                    wire:click='rejectLoan' />
+                @endcan
 
 
+                @else
 
-        @endif
+                @if($this->loan->status =='rejected')
+                <x-icon name="o-x-mark" class="text-2xl text-red-500 w-9 h-9" label="{{  $this->loan->status }}" />
 
-    </x-slot:actions>
+                @else
 
-    </x-header>
+                <x-icon name="o-check" class="text-2xl text-green-500 w-9 h-9" label="{{  $this->loan->status }}" />
 
-    <div class="grid grid-cols-4">
-
-
-        <x-stat
-        title="Loan Number"
-        value="{{ $this->loan->loan_application_no  }}"
-        />
-        <x-stat
-        title="Member"
-        value="{{ $this->loan->user->name  }}"
-
-
-        />
-
-        <x-stat
-        title="Loan Amount"
-        value="{{ number_format($this->loan->principal_amount ,2)}}"
-        />
-        <x-stat
-        title="Monthly Payment"
-        value="{{ number_format($this->loan->monthly_payment ,2)}}"
-        description='Rate: {{ $this->loan->monthly_interest_rate }}'
-        />
+                @endif
 
 
 
+                @endif
+
+            </x-slot:actions>
+
+        </x-header>
+
+        <div class="grid grid-cols-4">
+
+
+            <x-stat title="Loan Number" value="{{ $this->loan->loan_application_no  }}" />
+            <x-stat title="Member" value="{{ $this->loan->user->name  }}" />
+
+            <x-stat title="Loan Amount" value="{{ number_format($this->loan->principal_amount ,2)}}" />
+            <x-stat title="Monthly Payment" value="{{ number_format($this->loan->monthly_payment ,2)}}"
+                description='Rate: {{ $this->loan->monthly_interest_rate }}' />
+
+
+
+        </div>
     </div>
 
 
+    <div class="p-5 mt-3 border">
+        <x-header title="Payment Schedule"  size='text-xl' separator/>
     <x-table :headers="$headers" :rows="$loanItems" x-on:refresh-page.window="$wire.$refresh()" >
 
         @scope('cell_total_due', $loan)
@@ -151,31 +142,33 @@ new class extends Component {
 
             <span>{{ $loan->total_due }} </span>
             @if($loan->penalty >0)
-                <span class="text-red-500">({{ ($loan->penalty) }} )</span>
+            <span class="text-red-500">({{ ($loan->penalty) }} )</span>
             @endif
         </div>
 
         @endscope
         @scope('cell_status', $loan)
-            @if($loan->status=='to pay')
-            <x-badge :value="$loan->status" class="badge-info" />
+        @if($loan->status=='to pay')
+        <x-badge :value="$loan->status" class="badge-info" />
 
-            <x-button x-on:click="$dispatch('add-payment',{loanItemId:{{ $loan->id }}})" icon="o-currency-dollar" class="btn-sm btn-info"/>
+        <x-button x-on:click="$dispatch('add-payment',{loanItemId:{{ $loan->id }}})" icon="o-currency-dollar"
+            class="btn-sm btn-info" />
 
-            @elseif($loan->status=='overdue')
-            <x-badge :value="$loan->status" class="badge-error" />
-            @elseif($loan->status=='paid')
-                <x-badge :value="$loan->status" class="badge-success" />
+        @elseif($loan->status=='overdue')
+        <x-badge :value="$loan->status" class="badge-error" />
+        @elseif($loan->status=='paid')
+        <x-badge :value="$loan->status" class="badge-success" />
 
-            @else
-            <x-badge :value="$loan->status" class="badge-warning" />
+        @else
+        <x-badge :value="$loan->status" class="badge-warning" />
 
-            @endif
+        @endif
         @endscope
     </x-table>
 
+    </div>
 
-    <livewire:admin.loan.loan-payment-modal/>
+    <livewire:admin.loan.loan-payment-modal />
 
 
 </div>
