6f40e807c786799f9f291c3e0fbbfc2d67cfba04
soybean15 <marlonpadilla1593@gmail.com>
2025-01-04 23:06:38 +0800

    Update - remove cdn tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>QuickLoan - Fast and Easy Lending</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+  
     <script>
         tailwind.config = {
             theme: {
