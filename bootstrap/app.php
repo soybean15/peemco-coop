829f89437b2f214a1f7e6567314b86188040d686
soybean15 <marlonpadilla1593@gmail.com>
2025-01-05 20:30:51 +0800

    Update added middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -31,6 +31,17 @@ return Application::configure(basePath: dirname(__DIR__))
 
 
         ]);
+
+        $middleware->use([
+            \Illuminate\Foundation\Http\Middleware\InvokeDeferredCallbacks::class,
+            // \Illuminate\Http\Middleware\TrustHosts::class,
+            \Illuminate\Http\Middleware\TrustProxies::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
+            \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
+            \Illuminate\Http\Middleware\ValidatePostSize::class,
+            \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
+            \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
